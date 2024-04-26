f6b7fc07828c608189067d1887f524500997dec1
potentialadger <aa00516838@gmail.com>
2024-04-26 11:07:32 +0800

    更新了eeit79test的檔案

--- SB/src/main/java/com/jay/testEEIT179.java
+++ SB/src/main/java/com/jay/testEEIT179.java
@@ -3,7 +3,14 @@ package com.jay;
 public class testEEIT179 {
 
 	public testEEIT179() {
-		
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
+		System.out.println("askdljalsdjalsdj");
 	}
 
 }
