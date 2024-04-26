0782727e294fe488e7a541115648e8004b5a20e8
yuhaoyuhaoyuhaoyuhaoyuhao <h60915@gmail.com>
2024-04-26 11:01:57 +0800

    新增了OOOOOOOOOOOOOO

--- SB/src/main/java/com/yuhao/Yuhao.java
+++ SB/src/main/java/com/yuhao/Yuhao.java
@@ -3,7 +3,7 @@ package com.yuhao;
 public class Yuhao {
 
 	public static void main(String[] args) {
-		System.out.println("helloooooooooooooooo");
+		System.out.println("hellooooooooooooooooOOOOOOOOOOOOO");
 	}
 
 }
