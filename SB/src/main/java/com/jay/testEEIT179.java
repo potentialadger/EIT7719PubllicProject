497242fa5243e81853cc6a8072db34f378a657e4
Henry <lowel70501@gmail.com>
2024-04-26 11:07:01 +0800

    我新增了好多行err

--- SB/src/main/java/com/jay/testEEIT179.java
+++ SB/src/main/java/com/jay/testEEIT179.java
@@ -3,7 +3,52 @@ package com.jay;
 public class testEEIT179 {
 
 	public testEEIT179() {
-		
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
+		System.err.println("6666666666666");
 	}
 
 }
