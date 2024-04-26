5d083a78782a1be524d5c71f7518ae9da61a0d9c
chienchien6 <128012103+chienchien6@users.noreply.github.com>
2024-04-26 10:54:05 +0800

    gogo

--- SB/src/main/java/com/jay/EEIT79.java
+++ SB/src/main/java/com/jay/EEIT79.java
@@ -3,8 +3,8 @@ package com.jay;
 public class EEIT79 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.print("HeeeeeeeHooooooooooooo!!");
 	}
 
 }
